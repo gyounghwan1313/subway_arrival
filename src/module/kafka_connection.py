79cf4b99c1026d0ff108ba71672feda04ac9910e
jayden <jayden@acryl.ai>
2023-11-24 15:44:40 +0900

    kafka send logging update

--- src/module/kafka_connection.py
+++ src/module/kafka_connection.py
@@ -26,10 +26,11 @@ class KafkaConnector():
 
     def send_to_topic(self, topic: str, msg: Any) -> None:
         try:
-            logger.info(f"Topic : {topic} / msg : {msg}")
+            logger.info(f"Topic : {topic} / msg : {msg[:50]}.....")
             self._producer.send(topic, msg).get(timeout=5)
             logger.info("Send End")
         except Exception as e:
             logger.error(e)
+            logger.error(f"Fail message : {msg}")
             traceback.format_exc()
             sys.exit(1)

fb8688a3633906f4d3aacc3f448d4cc6aa8c9fe7
jayden <jayden@acryl.ai>
2023-12-05 10:53:55 +0900

    API ERROR Exception

--- src/collect_data/realtime.py
+++ src/collect_data/realtime.py
@@ -1,4 +1,3 @@
-
 import os
 import sys
 from typing import Optional, Union, Dict, List
@@ -26,10 +25,12 @@ class CollectPublicData(object):
     공공 데이터 수집 Class
     """
 
-    def __init__(self,
-                 broker: Union[str, List[str]] = None,
-                 aws_access_key_id: Optional[str] = None,
-                 aws_secret_access_key: Optional[str] = None):
+    def __init__(
+        self,
+        broker: Union[str, List[str]] = None,
+        aws_access_key_id: Optional[str] = None,
+        aws_secret_access_key: Optional[str] = None,
+    ):
         """
         :param broker: : 데이터를 전송할 Broker 주소를 입력받음
         """
@@ -44,9 +45,11 @@ class CollectPublicData(object):
         self.__aws_secret_access_key = aws_secret_access_key
 
         if aws_access_key_id and aws_secret_access_key:
-            self.__s3_client = boto3.client("s3",
-                                            aws_access_key_id=self.__aws_access_key_id,
-                                            aws_secret_access_key=self.__aws_secret_access_key)
+            self.__s3_client = boto3.client(
+                "s3",
+                aws_access_key_id=self.__aws_access_key_id,
+                aws_secret_access_key=self.__aws_secret_access_key,
+            )
         else:
             self.__s3_client = None
 
@@ -64,7 +67,7 @@ class CollectPublicData(object):
 
         return self._result
 
-    def call(self, url, call_count=0) -> Optional[Dict]:
+    def call(self, url, call_count=0) -> Union[Dict, bool]:
         if call_count >= 2:
             return None
 
@@ -74,15 +77,18 @@ class CollectPublicData(object):
         # {'status': 500, 'code': 'INFO-200', 'message': '해당하는 데이터가 없습니다.', 'link': '', 'developerMessage': '', 'total': 0}
 
         if self._get_data_json:
-            if 'errorMessage' not in self._get_data_json:
+            if "errorMessage" not in self._get_data_json:
                 logger.info("==ERROR==")
                 if "code" not in self._get_data_json:
                     logger.info("==Code key is Not Found")
                     return None
+                # 데이터 없음 에러 정상 처리
+                elif self._get_data_json["code"] == "INFO-200":
+                    return False
                 return None
             else:
                 return self._get_data_json
-        else: # 응답값이 없으면 : 에러가 발생함 -> 2번 더 호출하고 실패하면 멈춤
+        else:  # 응답값이 없으면 : 에러가 발생함 -> 2번 더 호출하고 실패하면 멈춤
             logger.info("Retry")
             logger.info(f"{call_count+1}")
             time.sleep(10)
@@ -90,7 +96,7 @@ class CollectPublicData(object):
 
     def _transform_to_pdf(self, data_key: str) -> None:
         self._pd_data = pd.DataFrame(self._get_data_json[data_key])
-        self._pd_data['time'] = self._now
+        self._pd_data["time"] = self._now
         logger.info(f"Data Count : {len(self._pd_data)}")
 
     def transform(self, data_key: str) -> None:
@@ -101,8 +107,8 @@ class CollectPublicData(object):
         :return:
         """
         self._json_data = self._get_data_json[data_key]
-        [i.update({"time": str(self._now)}) for i in self._json_data]
         logger.info(f"Data Count : {len(self._json_data)}")
+        [i.update({"time": str(self._now)}) for i in self._json_data]
 
     def _save_pdf(self, save_dir_path: str) -> str:
         file_path = f"{save_dir_path}{self._now.strftime('%Y-%m-%d-%H-%M-%S')}.parquet"
@@ -110,7 +116,9 @@ class CollectPublicData(object):
         logger.info(file_path)
         return file_path
 
-    def _transfer_s3(self, local_file_path: str, bucket: str, save_as_path: str) -> None:
+    def _transfer_s3(
+        self, local_file_path: str, bucket: str, save_as_path: str
+    ) -> None:
         if self.__s3_client:
             self.__s3_client.upload_file(local_file_path, bucket, save_as_path)
         else:
@@ -130,23 +138,26 @@ class CollectPublicData(object):
         kafka_conn = KafkaConnector(broker=self.__broker)
         if unit:
             for start_idx in range(0, len(self._json_data), unit):
-                kafka_conn.send_to_topic(topic=topic, msg=self._json_data[start_idx:start_idx+unit])
+                kafka_conn.send_to_topic(
+                    topic=topic, msg=self._json_data[start_idx : start_idx + unit]
+                )
         else:
             kafka_conn.send_to_topic(topic=topic, msg=self._json_data)
 
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     key = os.environ["api_key"]
     broker_1 = os.environ["KAFKA_BROKER_1_PRIVATE"]
     broker_2 = os.environ["KAFKA_BROKER_2_PRIVATE"]
     broker_3 = os.environ["KAFKA_BROKER_3_PRIVATE"]
-    position_topic = os.environ['KAFKA_POSITION_TOPIC']
-    arrival_topic = os.environ['KAFKA_ARRIVAL_TOPIC']
+    position_topic = os.environ["KAFKA_POSITION_TOPIC"]
+    arrival_topic = os.environ["KAFKA_ARRIVAL_TOPIC"]
 
     logger_class = LoadLogger()
     logger = logger_class.time_rotate_file(log_dir="/log/", file_name=f"realtime.log")
-    logger.info(f"""===========ENV===========\n Key : {key} \n Broker :{broker_1} / {broker_2} / {broker_3} \n Topics : {position_topic} / {arrival_topic} \n =========================""")
+    logger.info(
+        f"""===========ENV===========\n Key : {key} \n Broker :{broker_1} / {broker_2} / {broker_3} \n Topics : {position_topic} / {arrival_topic} \n ========================="""
+    )
 
     try:
         while True:
@@ -156,25 +167,41 @@ if __name__ == '__main__':
             position = CollectPublicData(broker=[broker_1, broker_2, broker_3])
             arrival = CollectPublicData(broker=[broker_1, broker_2, broker_3])
 
+            logger.info("##### Position START #####")
             logger.info("===API CALL START===")
-            _ = position.call(url=f"http://swopenAPI.seoul.go.kr/api/subway/{key}/json/realtimePosition/0/100/1호선")
-            _ = arrival.call(url=f"http://swopenAPI.seoul.go.kr/api/subway/{key}/json/realtimeStationArrival/ALL")
+            position_result = position.call(
+                url=f"http://swopenAPI.seoul.go.kr/api/subway/{key}/json/realtimePosition/0/100/1호선"
+            )
             logger.info("===API CALL END===")
+            if position_result:  # TRUE면
+                logger.info("===TRANSFORM START===")
+                position.transform(data_key="realtimePositionList")
+                logger.info("===Kafka Connect Start===")
+                position.producing_kafka(topic=position_topic, unit=None)
+            else:
+                logger.info("##### API Expected Error #####")
+            logger.info("##### Position END #####")
 
-            logger.info("===TRANSFORM START===")
-            position.transform(data_key='realtimePositionList')
-            arrival.transform(data_key='realtimeArrivalList')
-
-            logger.info("===Kafka Connect Start===")
-            position.producing_kafka(topic=position_topic, unit=None)
-            arrival.producing_kafka(topic=arrival_topic, unit=100)
-
-            logger.info("#################### DONE ####################")
+            logger.info("##### Arrival #####")
+            logger.info("===API CALL START===")
+            arrival_result = arrival.call(
+                url=f"http://swopenAPI.seoul.go.kr/api/subway/{key}/json/realtimeStationArrival/ALL"
+            )
+            logger.info("===API CALL END===")
+            if arrival_result:
+                logger.info("===TRANSFORM START===")
+                arrival.transform(data_key="realtimeArrivalList")
+                logger.info("===Kafka Connect Start===")
+                arrival.producing_kafka(topic=arrival_topic, unit=100)
+                logger.info("#################### DONE ####################")
+            else:
+                logger.info("##### API Expected Error #####")
+            logger.info("##### Position END #####")
 
             time.sleep(60)
     except Exception as e:
         logger.error(e)
         logger.error(traceback.format_exc())
-        logger.info(f"position._get_data_json {position._get_data_json}")
-        logger.info(f"arrvial._get_data_json {arrival._get_data_json}")
+        logger.info(f"position._get_data_json : {position._get_data_json}")
+        logger.info(f"arrvial._get_data_json : {arrival._get_data_json}")
         sys.exit(1)
